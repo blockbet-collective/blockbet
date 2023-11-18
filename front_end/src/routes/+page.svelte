c0127bb669dc85b30def36ed37d222d65c8855cc
Ben Riazy <hamon.riazy@gmail.com>
2023-11-18 08:40:24 +0300

    feat: more chains

--- front_end/src/routes/+page.svelte
+++ front_end/src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Badge, Button } from 'flowbite-svelte';
 	import { signerAddress, loading, chainId, wagmiLoaded, connected, web3Modal } from 'svelte-wagmi';
 	import { configureWagmi, disconnectWagmi } from 'svelte-wagmi';
+	import { useNetwork } from '@wagmi/core';
 
 	if (browser) {
 		configureWagmi({
@@ -29,8 +30,36 @@
 				return 'Rinkeby';
 			case 5:
 				return 'Goerli';
+			case 11155111:
+				return 'Sepolia';
 			case 42:
 				return 'Kovan';
+			case 59140:
+				return 'Linea Testnet';
+			case 59144:
+				return 'Linea';
+			case 8453:
+				return 'Base';
+			case 84531:
+				return 'Base Goerli Testnet';
+			case 84532:
+				return 'Base Sepolia Testnet';
+			case 88888:
+				return 'Chiliz';
+			case 88880:
+				return 'Chiliz Scoville Testnet';
+			case 534352:
+				return 'Scroll';
+			case 534351:
+				return 'Scroll Sepolia Testnet';
+			case 1442:
+				return 'Polygon zkEVM Testnet';
+			case 1101:
+				return 'Polygon zkEVM';
+			case 42161:
+				return 'Arbitrum One';
+			case 421614:
+				return 'Arbitrum Sepolia';
 			default:
 				return 'Unknown';
 		}
