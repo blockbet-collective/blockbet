eb026d6fa921df1e1705ca8b28e7d70560f5eb1e
Ben Riazy <hamon.riazy@gmail.com>
2023-11-18 12:54:19 +0300

    feat: Pool pag

--- front_end/src/routes/liquidity/+page.svelte
+++ front_end/src/routes/liquidity/+page.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
-	import { Card } from 'flowbite-svelte';
+	import { Button, Card, Input } from 'flowbite-svelte';
+	import { ChartSolid, FaceGrinSolid } from 'flowbite-svelte-icons';
 </script>
 
-<div class="flex flex-col items-center space-y-4 m-10">
-	<Card>Max Bet</Card>
-	<Card>TVL</Card>
+<div class="flex items-center justify-center space-x-4 m-10">
+	<Card>
+		<ChartSolid class="w-7 h-7 mb-3 text-gray-500 dark:text-gray-400" />
+		<h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">TVL</h5>
+		<p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+			The treasury currently holds the displayed amount of liquidity in the bank
+		</p>
+		<h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">42 eth</h5>
+	</Card>
+	<Card
+		><FaceGrinSolid class="w-7 h-7 mb-3 text-gray-500 dark:text-gray-400" />
+		<h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+			Max Bet
+		</h5>
+		<p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+			The maximum bet possible based on current total liquidity
+		</p>
+		<h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">2.1 eth</h5>
+	</Card>
 </div>
